3f46264ccecb303b0138b08828e75ca5d59e086a
Diogo silva <107317031+Diog0silva@users.noreply.github.com>
2025-06-30 23:15:31 -0300

    Update wpp.js

--- js/wpp.js
+++ js/wpp.js
@@ -1,5 +1,5 @@
 function abrirWpp(servico) {
-    const numero = "5511999999999"; // Altere para seu número com DDD e país
+    const numero = "5521979839083"; 
     const mensagem = `Olá! Gostaria de agendar o serviço: *${servico}*`;
     const url = `https://api.whatsapp.com/send?phone=${numero}&text=${encodeURIComponent(mensagem)}`;
     window.open(url, '_blank');
